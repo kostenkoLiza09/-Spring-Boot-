9d2fb5f3894e50a8baab141947a344a913ce5b8c
kostenkoLiza09 <kostaz063@gmail.com>
2025-04-15 16:01:00 +0200

    add delete Messages and find By Id

--- src/main/java/com/example/startspring/MessageController.java
+++ src/main/java/com/example/startspring/MessageController.java
@@ -15,9 +15,23 @@ public class MessageController {
     public List<Message> getMessages() {
         return messages;
     }
+
     @PostMapping
     public String addMessage(@RequestBody Message message) {
         messages.add(message);
         return message.getName();
     }
+
+    @GetMapping("{/id}")
+    public String findById(@PathVariable String id) {
+        return id;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMessages(@PathVariable String id){
+        boolean delete = messages.removeIf(message -> message.getId().equals(id));
+        return delete ? "deleted" : "not found";
+    }
+
+
 }
